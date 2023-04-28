a85216b8f4dcf3580b4890eb26c48432849ce0a2
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-04-28 16:58:03 +0300

    Added Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import HomePage from "./Components/HomePage/HomePage";
-import Login from "./Components/Login/Login";
+import Login from "./Components/Register/Login";
 import Footer from "./Components/Footer/Footer";
 
 export default function App() {
@@ -11,18 +11,11 @@ export default function App() {
         <Routes>
           <Route element={<Navbar />}>
             <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<Login />} />
             {/* <Route path="*" element={<PageNotFound />} /> */}
           </Route>
-        </Routes>
-        {/* <Routes>
-        <Route path="/" element={<Footer />}>
-          <Route path="/home" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
-        </Route>
-      </Routes> */}
+        </Routes>
       </Router>
-      <Footer />
     </>
   );
 }
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./Hero";
 import Second from "./Second";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 import "./homepage.css";
 export default function HomePage() {
   // const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ export default function HomePage() {
     <div>
       <Hero />
       <Second />
+      <Footer />
     </div>
   );
 }
