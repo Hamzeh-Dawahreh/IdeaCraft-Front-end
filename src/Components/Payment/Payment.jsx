a088aef35e2d6cf54807ecdb07039af1a76ae58f
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-04-29 19:25:15 +0300

    Added payment page

--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -8,7 +8,26 @@ export default function Payment() {
   return (
     <>
       <Navbar />
-      <h1>Hello!</h1>
+      <div className="payment-card">
+        <div class="total-payment">
+          <h5>Total</h5>
+          <h6>$19.99</h6>
+        </div>
+        <hr />
+
+        <div className="info-container">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="Card Number" />
+          <input type="text" placeholder="CVV" />
+          <img src="./src/Images/payment-methods.png" />
+
+          <input type="text" placeholder="MM/YY" />
+        </div>
+        <div>
+          <button>Submit Paymen</button>
+        </div>
+      </div>
       <Footer />
     </>
   );
