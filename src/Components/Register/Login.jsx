dc3b913ca9a20ddbab04bce276da8af23c4e3d31
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-05-14 23:34:00 +0300

    Login page

--- src/Components/Register/Login.jsx
+++ src/Components/Register/Login.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 import "./register.css";
 export default function Login() {
+  const [formValues, setFormValues] = useState();
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormValues((prevValues) => {
+      return {
+        ...prevValues,
+        [name]: value,
+      };
+    });
+  }
+  function handleSubmit() {
+    //fdsokfopdsf
+    event.preventDefault();
+    localStorage.setItem("email", JSON.stringify(email));
+  }
   return (
     <>
       <div className="register-body">
@@ -20,6 +35,8 @@ export default function Login() {
                 className="login-input"
                 type="text"
                 placeholder="Username"
+                onChange={handleChange}
+                value={formValues.email}
               />
               <i className="bi bi-person-fill">
                 <svg
@@ -40,6 +57,8 @@ export default function Login() {
                   className="login-input"
                   type="password"
                   placeholder="Password"
+                  onChange={handleChange}
+                  value={formValues.password}
                 />
                 <i className="bi bi-lock-fill">
                   {" "}
@@ -97,7 +116,9 @@ export default function Login() {
                 <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
               </svg>
             </div>
-            <button className="login-button">Login</button>
+            <button className="login-button" onClick={handleClick}>
+              Login
+            </button>
           </div>
         </div>
       </div>
